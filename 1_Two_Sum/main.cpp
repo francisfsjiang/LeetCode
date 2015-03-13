d11932705fd81d7941141678985a4f75037e8393
Neveralso <neveralso@gmail.com>
2015-03-13 21:49:17 +0800

    commit

---
+++ 1_Two_Sum/main.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int bs(const vecotr<int>& arr, int low, int high, int key){
+        int mid = low + (high - low) >> 1;
+        if (low > high) {
+            return false;
+        }
+        else {
+                if (arr[mid] == key) {
+                    return mid;
+                }
+                else if (arr[mid] > key) {
+                    return bs(arr, low, mid-1, key);
+                }
+                else {
+                    return bs(arr, mid+1, high, key);
+                }
+        }
+    }
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        sort(numbers.begin(), numbers.end());
+        for (int i=0; i<numbers.size()-1; ++i) {
+            int another_num =
+                bs(numbers, 0, numbers.size()-1, target-numbers[i]);
+            if (another_num) {
+                return vector<int>{i, another_num};
+            }
+        }
+    }
+};
+
+int main() {
+
+}
---
+++ base.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+
+
+int main() {
+
+}
