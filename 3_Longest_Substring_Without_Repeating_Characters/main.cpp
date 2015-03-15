a41bdf2cf31333956838eb0c8356f051bb928949
Neveralso <neveralso@gmail.com>
2015-03-15 19:07:48 +0800

    commit

--- 3_Longest_Substring_Without_Repeating_Characters/main.cpp
+++ 3_Longest_Substring_Without_Repeating_Characters/main.cpp
@@ -10,8 +10,42 @@
 
 using namespace std;
 
-
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> alpha(256,0);
+        int head=0,tail=0,len=(int)s.size();
+        string lst="";
+        while (head<len) {
+            if (alpha[s[head]]==0) {
+                alpha[s[head]]=1;
+                ++head;
+            }
+            else {
+                if (head-tail > lst.size()) {
+                    lst=string(s,tail,head-tail);
+                }
+                //debug(lst)
+                while(alpha[s[head]]==1 && tail<head) {
+                    alpha[s[tail]]=0;
+                    ++tail;
+                }
+            }
+            if (head==len) {
+                if (head-tail > lst.size()) {
+                    lst=string(s,tail,head-tail);
+                }
+                break;
+            }
+        }
+        //debug(lst);
+        return lst.size();
+    }
+};
 
 int main() {
+    Solution sol;
+    int len=sol.lengthOfLongestSubstring("aaaaa#@*)!%*#@!)&%)@!#(^$*\"\"\"\"dazcgrf");
+    debug(len)
     return 0;
 }
